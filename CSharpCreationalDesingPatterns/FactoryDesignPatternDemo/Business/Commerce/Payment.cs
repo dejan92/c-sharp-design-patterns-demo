25b594d123540b3d7eb2a7d69253114b25eee05b
Dejan Jovanov <dejan.jovanov@opengi.mk>
2023-01-25 09:08:09 +0100

    FactoryPattern first flavor

---
+++ CSharpCreationalDesingPatterns/FactoryDesignPatternDemo/Business/Commerce/Payment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPatternDemo.Business.Commerce
+{
+    public class Payment
+    {
+        public decimal Amount { get; set; }
+        public PaymentProvider PaymentProvider { get; set; }
+    }
+}
---
+++ CSharpCreationalDesingPatterns/FactoryDesignPatternDemo/Business/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿using FactoryDesignPatternDemo.Business.Commerce;
+using FactoryDesignPatternDemo.Business.Shipping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPatternDemo.Business
+{
+    public class ShoppingCart
+    {
+        private readonly Order order;
+
+        public ShoppingCart(Order order)
+        {
+            this.order = order;
+        }
+
+        public string Finalize()
+        {
+            var shippingProvider = ShippingProviderFactory.CreateShippingProvider(order.Sender.Country);
+
+            order.ShippingStatus = ShippingStatus.ReadyForShippment;
+
+            return shippingProvider.GenerateShippingLabelFor(order);
+        }
+    }
+
+}
