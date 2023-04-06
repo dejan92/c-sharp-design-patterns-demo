70813c52983d485e4238bb7b33423e0707e05293
Dejan Jovanov <dejan.jovanov@opengi.mk>
2023-04-06 09:09:07 +0200

    Buider pattern init

---
+++ CSharpCreationalDesingPatterns/BuilderPatternDemo/InventoryReport.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatternDemo
+{
+    public class FurnitureItem
+    {
+        public string Name;
+        public double Price;
+        public double Height;
+        public double Width;
+        public double Weight;
+
+        public FurnitureItem(string productName, double price, double height, double width, double weight)
+        {
+            this.Name = productName;
+            this.Price = price;
+            this.Height = height;
+            this.Width = width;
+            this.Weight = weight;
+        }
+    }
+
+    public class InventoryReport
+    {
+        public string TitleSection;
+        public string DimensionsSection;
+        public string LogisticsSection;
+
+        public string Debug()
+        {
+            return new StringBuilder()
+                .AppendLine(TitleSection)
+                .AppendLine(DimensionsSection)
+                .AppendLine(LogisticsSection)
+                .ToString();
+        }
+    }
+
+    public interface IFurnitureInventoryBuilder
+    {
+        void AddTitle();
+        void AddDimensions();
+        void AddLogistics();
+        InventoryReport GetDailyReport();
+    }
+
+    public class DailyReportBuilder : IFurnitureInventoryBuilder
+    {
+        private InventoryReport _report;
+        private IEnumerable<FurnitureItem> _items;
+
+        public DailyReportBuilder(IEnumerable<FurnitureItem> items)
+        {
+            Reset();
+            _items = items;
+        }
+
+        public void AddTitle()
+        {
+            _report.TitleSection = "----- Daily Inventory Report -----\n\n";
+        }
+
+        public void AddDimensions()
+        {
+            _report.DimensionsSection = string.Join(Environment.NewLine, _items.Select(product => 
+            $"Product: {product.Name} \n" +
+            $"Price: {product.Height} \n" +
+            $"Height: {product.Height} x Width: {product.Width} -> \n" +
+            $"Weight: {product.Weight}"));
+        }
+
+        public void AddLogistics(DateTime dateTime)
+        {
+            _report.LogisticsSection = $"Report generated on: {dateTime}";
+        }
+
+        public InventoryReport GetDailyReport()
+        {
+            InventoryReport finishedReport = _report;
+            Reset();
+            return finishedReport;
+        }
+
+        public void Reset()
+        {
+            _report = new InventoryReport();
+        }
+
+
+    }
+}
