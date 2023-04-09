ae8285373dd55e6990b21749a0fd405d79bf8dcd
Dejan Jovanov <dejan.jovanov@opengi.mk>
2023-04-09 12:58:13 +0200

    Refactor builder pattern demo to use more real examples

---
+++ CSharpCreationalDesingPatterns/BuilderPatternDemo/BedroomBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderPatternDemo
+{
+    public class BedroomBuilder : IRoomBuilder
+    {
+        private Room _room = new Room();
+
+        public void SetSize(int size)
+        {
+            _room.Size = size;
+        }
+
+        public void SetColor(string color)
+        {
+            _room.Color = color;
+        }
+
+        public void SetFlooring(string flooring)
+        {
+            _room.Flooring = flooring;
+        }
+
+        public void SetLighting(string lighting)
+        {
+            _room.Lighting = lighting;
+        }
+
+        public void AddOptionalFeature(string feature)
+        {
+            _room.OptionalFeatures.Add(feature);
+        }
+
+        public Room GetResult()
+        {
+            return _room;
+        }
+    }
+
+}
--- CSharpCreationalDesingPatterns/BuilderPatternDemo/Program.cs
+++ CSharpCreationalDesingPatterns/BuilderPatternDemo/Program.cs
@@ -7,14 +7,61 @@ namespace BuilderPatternDemo
     {
         static void Main(string[] args)
         {
-            var items = new List<FurnitureItem>
-            {
-                new FurnitureItem("Sectional Couch", 55.5, 22.4, 78.6, 35.0),
-                new FurnitureItem("Nightstand", 25.0, 12.4, 20.0, 10.0),
-                new FurnitureItem("Dining Table", 105.0, 35.4, 100.6, 55.5),
-            };
+        
+            var livingRoomBuilder = new LivingRoomBuilder();
+            livingRoomBuilder.SetSize(20);
+            livingRoomBuilder.SetColor("blue");
+            livingRoomBuilder.SetFlooring("carpet");
+            livingRoomBuilder.SetLighting("chandelier");
+            livingRoomBuilder.AddOptionalFeature("fireplace");
+            var livingRoom = livingRoomBuilder.GetResult();
+            OutputRoomProperties(livingRoom);
+
+            var bedroomBuilder = new BedroomBuilder();
+            bedroomBuilder.SetSize(15);
+            bedroomBuilder.SetColor("green");
+            bedroomBuilder.SetFlooring("hardwood");
+            bedroomBuilder.SetLighting("ceiling fan");
+            bedroomBuilder.AddOptionalFeature("walk-in closet");
+            bedroomBuilder.AddOptionalFeature("bedroom TV");
+            var bedroom = bedroomBuilder.GetResult();
+            OutputRoomProperties(bedroom);
+
+            var bathroomBuilder = new BathroomBuilder();
+            bathroomBuilder.SetSize(10);
+            bathroomBuilder.SetColor("white");
+            bathroomBuilder.SetFlooring("tile");
+            bathroomBuilder.SetLighting("recessed lights");
+            bathroomBuilder.AddOptionalFeature("double sink");
+            var bathroom = bathroomBuilder.GetResult();
+            OutputRoomProperties(bathroom);
 
 
+            Console.ReadLine();
         }
+
+        public static void OutputRoomProperties(Room room)
+        {
+            Console.WriteLine($"Room size: {room.Size}");
+            Console.WriteLine($"Room color: {room.Color}");
+            Console.WriteLine($"Room flooring: {room.Flooring}");
+            Console.WriteLine($"Room lighting: {room.Lighting}");
+
+            if (room.OptionalFeatures.Count > 0)
+            {
+                Console.WriteLine("Optional features:");
+                foreach (var feature in room.OptionalFeatures)
+                {
+                    Console.WriteLine($"- {feature}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No optional features.");
+            }
+
+            Console.WriteLine("- - - - - - -");
+        }
+
     }
 }
